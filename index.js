7d90f6ee6b6fb0bc69e67b05aa45a16e1cbdb690
joydotcom <msjoyjackson@gmail.com>
2021-04-08 17:31:18 -0400

    added readme files, and read me input values

---
+++ index.js
@@ -0,0 +1,83 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = (answers) =>
+  `## TITLE
+
+  ## DESCRIPTION
+  
+  ## TABLE OF CONTENTS
+  
+  * [INSTALLATION] (#usage)
+  * [USAGE] (#usage)
+  * [LICENSE] (#license)
+  * [CONTRIBUTING] (#contributing)
+  * [TESTS] (#test)
+  * [QUESTIONS] (#questions)
+  
+  ## INSTALLATION
+  
+  ## USAGE
+  
+  ## LICENSE
+  
+  ## CONTRIBUTING
+  
+  ## TESTS
+  
+  ## QUESTIONS `;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?',
+    },
+    {
+      type: 'input',
+      name: 'location',
+      message: 'Where are you from?',
+    },
+    {
+      type: 'input',
+      name: 'hobby',
+      message: 'What is your favorite hobby?',
+    },
+    {
+      type: 'input',
+      name: 'food',
+      message: 'What is your favorite food?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username',
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Enter your LinkedIn URL.',
+    },
+  ])
+  .then((answers) => {
+    const htmlPageContent = generateHTML(answers);
+
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+  });
