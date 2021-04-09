99595c8f2f2a0118420d011ad99004d5b15cf73f
joydotcom <msjoyjackson@gmail.com>
2021-04-08 20:43:22 -0400

    added links in the license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+    return license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+  }
+  else if (license === "Apache 2.0") {
+return license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]"
+
+
+  } else return ""
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return license = "(https://opensource.org/licenses/MIT)"
+  } else if (license === "Apache 2.0") {
+return license = "(https://opensource.org/licenses/Apache-2.0)"
+
+
+  } else return ""
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -23,7 +44,7 @@ function generateMarkdown(data) {
   * [USAGE] (#usage)
   ${data.title}
   * [LICENSE] (#license)
-  ${data.license}
+  THis project is licensed with ${data.license}
   * [CONTRIBUTING] (#contributing)
   ${data.contributing}
   * [TESTS] (#test)
